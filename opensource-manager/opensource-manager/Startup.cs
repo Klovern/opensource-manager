03a95b3b4b52b625cf10a88e988b8f4909333c1a
Klovern <oskar.kindeland@gmail.com>
2017-10-15 18:27:21 +0200

    init MVC

---
+++ opensource-manager/opensource-manager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(opensource_manager.Startup))]
+namespace opensource_manager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
