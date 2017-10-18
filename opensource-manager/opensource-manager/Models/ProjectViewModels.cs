1e37467fd980a359b9fec1dbd90b301b0a30fd0f
Klovern <oskar.kindeland@gmail.com>
2017-10-18 22:57:59 +0200

    ProjectController +Create Project & ProjectUser - StoredProcedure

---
+++ opensource-manager/opensource-manager/Models/ProjectViewModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace opensource_manager.Models
+{
+    public class ProjectViewModels
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public class RegisterViewModel
+        {
+            [Required]
+            [Display(Name = "Title")]
+            public string String { get; set; }
+        }
+
+        public class ProjectUser
+        {
+            public string UserID { get; set; }
+            public string Role { get; set; }
+            public int ProjectId { get; set; }
+        }
+    }
+}
