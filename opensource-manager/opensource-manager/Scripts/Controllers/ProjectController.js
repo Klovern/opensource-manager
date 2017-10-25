e8ca24e8b1799a2573e03c5aa8eecd4912b31a1a
Klovern <oskar.kindeland@gmail.com>
2017-10-25 15:38:39 +0200

    ui

--- opensource-manager/opensource-manager/Scripts/Controllers/ProjectController.js
+++ opensource-manager/opensource-manager/Scripts/Controllers/ProjectController.js
@@ -1,8 +1,78 @@
 ﻿var ProjectController = function ($scope) {
-    $scope.models = {
-        helloAngular: "I work!"
+    $scope.cards = [];
+
+    $scope.init = function (json) {
+        // console.log(JSON.stringify(json.data.ResultList));
+
+        json.data.ResultList.forEach(function (item) {
+            console.log("hit");
+            $scope.cards.push(
+                {
+                    helloAngular: "I do not work!",
+                    BoardId: item.BoardId,
+                    Title: item.Title
+                }
+            );
+        });
+
     };
+
+    console.log($scope.cards);
+    setTimeout(function () {
+        console.log("update");
+        $scope.cards = [
+            {
+                helloAngular: "I do not work!",
+                BoardId: "1",
+                Title: "Todo"
+            },
+            {
+                helloAngular: "Iluul!",
+                BoardId: "32",
+                Title: "Test"
+            },
+            {
+                helloAngular: "I do not work!",
+                BoardId: "s",
+                Title: "Done"
+            },
+            {
+                helloAngular: "I do not work!",
+                BoardId: "s",
+                Title: "Extra"
+            }
+        ];
+        $scope.$digest();
+    }, 3000);
+
+    $scope.updateData = function () {
+        $scope.cards = [
+            {
+                helloAngular: "I do not work!",
+                BoardId: "1",
+                Title: "Todo"
+            },
+            {
+                helloAngular: "Iluul!",
+                BoardId: "32",
+                Title: "Test"
+            },
+            {
+                helloAngular: "I do not work!",
+                BoardId: "s",
+                Title: "Done"
+            },
+            {
+                helloAngular: "I do not work!",
+                BoardId: "s",
+                Title: "Extra"
+            },
+        ];
+        console.log($scope.cards);
+    }
 }
+
+
 
 // The $inject property of every controller (and pretty much every other type of object in Angular) needs to be a string array equal to the controllers arguments, only as strings
 ProjectController.$inject = ['$scope'];
