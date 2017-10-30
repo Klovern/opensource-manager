6b0ad32af56fd578521ea327f8d57b65bfbca5bf
Klovern <oskar.kindeland@gmail.com>
2017-10-30 22:06:43 +0100

    REWORK Sprints

---
+++ opensource-manager/opensource-manager/Controllers/SprintController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace opensource_manager.Controllers
+{
+    [RoutePrefix("project/{projectId}/sprint")]
+    public class SprintController : Controller
+    {
+        // GET: Sprint
+        [Route("Index")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("{sprintId}")]
+        public ActionResult Index(int sprintId)
+        {
+            Console.WriteLine("hello" + sprintId);
+            return View();
+        }
+    }
+}
--- opensource-manager/opensource-manager/Scripts/Controllers/ProjectController.js
+++ opensource-manager/opensource-manager/Scripts/Controllers/ProjectController.js
@@ -1,17 +1,34 @@
 ﻿var ProjectController = function ($scope) {
     $scope.cards = [];
-
     $scope.init = function (json) {
         json.data.ResultList.forEach(function (item) {
+            var n = 1;
             $scope.cards.push(
                 {
                     BoardId: item.BoardId,
                     ColumnId: item.ColumnId,
-                    Title: item.Title            
-                }
-            );
+                    Title: item.Title,
+                    selects: json.data.ResultList.forEach(function(select) {
+                        if (n === select.ColumnId) {
+                             selects.push({
+                                isSelected: "selected",
+                                value: select.ColumnId
+                            });
+                        } else {
+                                selects.push({
+                                isSelected: "",
+                                value: select.ColumnId
+                            });
+                        }
+                        
+                        n++;
+                    })
+                   
+                });
+    
+
         });
-    };  
+    };
 }
 
 // The $inject property of every controller (and pretty much every other type of object in Angular) needs to be a string array equal to the controllers arguments, only as strings
